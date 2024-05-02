3f359277c3c1b5546c0606ee1c80b6035eb690d3
Kacper Murygin <kacper.mur123@gmail.com>
2024-05-02 12:06:47 +0200

    Token - type hint for value param

--- src/token/token.py
+++ src/token/token.py
@@ -1,8 +1,9 @@
 from src.token.token_types import TokenType
+from typing import Optional
 
 
 class Token:
-    def __init__(self, type: TokenType, value, position: tuple[int, int]):
+    def __init__(self, type: TokenType, value: Optional[str | int | float], position: tuple[int, int]):
         self.type = type
         self.value = value
         self.position = position
