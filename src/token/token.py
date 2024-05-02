890a44236119da37acc6e2d04552d500251f88bb
Kacper Murygin <kacper.mur123@gmail.com>
2024-05-02 12:04:41 +0200

    Token class with str method and type hints

--- src/token/token.py
+++ src/token/token.py
@@ -1,5 +1,11 @@
+from src.token.token_types import TokenType
+
+
 class Token:
-    def __init__(self, type, value, position):
+    def __init__(self, type: TokenType, value, position: tuple[int, int]):
         self.type = type
         self.value = value
         self.position = position
+
+    def __str__(self):
+        return f'{self.type}: {self.value}'
