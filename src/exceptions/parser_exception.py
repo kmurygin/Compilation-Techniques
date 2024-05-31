09d87eb4e5182ce457ec7a8fda13902b708ef845
Kacper Murygin <kacper.mur123@gmail.com>
2024-05-31 16:39:48 +0200

    implemented parser exceptions

--- src/exceptions/parser_exception.py
+++ src/exceptions/parser_exception.py
@@ -1,16 +1,37 @@
 class ParserException(Exception):
-    def __init__(self, message, position):
-        self.message = message
+    def __init__(self, position):
         self.line = position[0]
         self.column = position[1]
-
-    def __str__(self):
-        return f"LexerException: {self.message} in line:{self.line} column:{self.column}"
 
 
 class SyntaxError(ParserException):
     def __init__(self, message, position):
-        super().__init__(message, position)
+        super().__init__(position)
+        self.message = message
 
     def __str__(self):
         return f"SyntaxError: Expected {self.message} in line:{self.line} column:{self.column}"
+
+
+class NoSemicolonError(ParserException):
+    def __init__(self, position):
+        super().__init__(position)
+
+    def __str__(self):
+        return f"NoSemicolonError: Missing semicolon in line:{self.line} column:{self.column}"
+
+
+class NoClosingCurlyBracketError(ParserException):
+    def __init__(self, position):
+        super().__init__(position)
+
+    def __str__(self):
+        return"NoClosingCurlyBracketError: Missing closing curly bracket in line:{self.line} column:{self.column}"
+
+
+class NoClosingBracketError(ParserException):
+    def __init__(self, position):
+        super().__init__(position)
+
+    def __str__(self):
+        return"NoClosingBracketError: Missing closing bracket in line:{self.line} column:{self.column}"
